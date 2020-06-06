c2c5424b7f47aa3ba846da589b5a78290fe23578
Nathan Landis <nathanlandis@gmail.com>
2020-06-06 15:46:39 -0500

    Two triangles render checkpoint

--- src/Mivi.Console/Program.cs
+++ src/Mivi.Console/Program.cs
@@ -21,22 +21,7 @@ namespace Mivi.Console
             Glfw.WindowHint(Hint.Decorated, true);
             Glfw.WindowHint(Hint.OpenglForwardCompatible, true);
 
-            using (var window = new NativeWindow(800, 600, "MIVI"))
-            {
-                // Main application loop
-                while (!window.IsClosing)
-                {
-                    // OpenGL rendering
-                    // Implement any timing for flow control, etc (see Glfw.GetTime())
-
-                    // Swap the front/back buffers
-                    window.SwapBuffers();
-
-                    // Poll native operating system events (must be called or OS will think application is hanging)
-                    Glfw.PollEvents();
-                }
-            }
-
+            TriangleProgram.EntryPoint(_);
         }
 
         public static async Task Main_Old(string[] args)
